ab28afeada9ac20423431df8dd914ce67ff9d10e
ebsare kadiri <ek48036@ubt-uni.net>
2026-01-16 15:08:22 +0100

    fix: remove secrets from code and use env variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { poolPromise } = require("./config/mssql");
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Smart University API running" });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query("SELECT GETDATE() AS now");
+    res.json(result.recordset[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = app;
